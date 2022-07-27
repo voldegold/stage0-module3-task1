de595ac4a5d9301f6dc19f36657ccf3d1de78ec5
voldegold <kva1107@gmail.com>
2022-07-28 00:17:41 +0600

    ok

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,7 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
+        System.out.println("java" + "\nis"  + "\nsmth");
         //write your code here
     }
 }
